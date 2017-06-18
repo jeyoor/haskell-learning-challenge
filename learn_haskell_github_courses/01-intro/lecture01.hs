df65850320ecb597b2e4f4d043c17cb89e1bdef7
Jeyan Oorjitham <jeyoor@gmx.com>
2017-06-17 22:02:59 -0400

    fixed most of the warnings

--- learn_haskell_github_courses/01-intro/lecture01.hs
+++ learn_haskell_github_courses/01-intro/lecture01.hs
@@ -1,3 +1,6 @@
+{-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE ViewPatterns, PatternSynonyms, RankNTypes #-}
+
 --note: currently experimenting by running `stack ghci` and loading this file with `:l lecture_tests.hs`
 --TODO: add quickcheck tests... maybe make this a module too
 
@@ -13,19 +16,52 @@ module Cis194.Lecture01 (
     hailstoneList,
     hailstoneSeq,
     intListLength,
+    intSeqLength,
+    intSeqSynonymLength,
+    intSeqLongFormLength,
+    sumEveryTwo,
+    sumSeqEveryTwo,
+    sumSeqLongFormEveryTwo,
+    hailstoneLen,
+    hailstoneSeqLen,
+    exp_result,
+    p,
+    nums,
+    range,
+    range2,
+    hello1,
+    hello2,
+    hello3,
+    helloSame,
+    hello23Same,
+    emptyList,
+    emptySeq,
+    ex18,
+    ex19,
+    ex20,
+    ex21,
+    ex18_seq,
+    ex19_seq,
+    ex20_seq,
+    ex21_seq,
     ) where
 
 import Data.Char (ord)
-import Data.Sequence (Seq, (<|), (|>), empty, singleton, )
-import qualified Data.Sequence as DS
+import qualified Data.Sequence as S
+import Data.Sequence (Seq, (<|), empty, singleton, viewl, ViewL(EmptyL))
 import qualified Data.Foldable as DF
+
+pattern Empty <- (viewl -> EmptyL)
+pattern x :< xs <- (viewl -> x S.:< xs)
 
 biggestInt, smallestInt :: Int
 biggestInt = maxBound
 smallestInt = minBound
 
-n :: Integer
-n = 2^2^2^2
+exp_result :: Integer
+base :: Integer
+base = 2
+exp_result = base^base^base^base
 
 sumtorial :: Integer -> Integer
 sumtorial 0 = 0
@@ -81,26 +117,34 @@ hello1 = ['h', 'e', 'l', 'l', 'o']
 hello2 :: String
 hello2 = "hello"
 
-hello3 :: DS.Seq Char
-hello3 = DS.fromList "hello"
+hello3 :: Seq Char
+hello3 = S.fromList "hello"
 
-
+helloSame :: Bool
 helloSame = hello1 == hello2
+hello23Same :: Bool
 hello23Same = hello1 == DF.toList hello3
 
 emptyList = []
 emptySeq = empty
 
+ex18 :: [Integer]
 ex18 = 1 : []
+ex19 :: [Integer]
 ex19 = 3 : (1 : [])
+ex20 :: [Integer]
 ex20 = 2 : 3 : 4 : []
+ex21 :: Bool
 ex21 = [2,3,4] == 2 : 3 : 4 : []
 
+ex18_seq :: Seq Integer
 ex18_seq = 1 <| empty
+ex19_seq :: Seq Integer
 ex19_seq = 3 <| (1 <| empty)
+ex20_seq :: Seq Integer
 ex20_seq = 2 <| 3 <| 4 <| empty 
-ex21_seq = DS.fromList [2,3,4] == 2 <| 3 <| 4 <| empty 
-
+ex21_seq :: Bool
+ex21_seq = S.fromList [2,3,4] == 2 <| 3 <| 4 <| empty 
 
 hailstoneList :: Integer -> [Integer]
 hailstoneList 1 = [1]
@@ -114,4 +158,48 @@ hailstoneSeq n = n <| hailstoneSeq (hailstone n)
 -- Compute the length of a list of Integers.
 intListLength :: [Integer] -> Integer
 intListLength []     = 0
-intListLength (x:xs) = 1 + intListLength xs
+intListLength (_:xs) = 1 + intListLength xs
+
+intSeqLength :: Seq Integer -> Integer
+intSeqLength (S.viewl -> S.EmptyL)     = 0
+intSeqLength (S.viewl -> (_ S.:< xs))  = 1 + intSeqLength xs
+intSeqLength _                         = 0
+
+intSeqSynonymLength :: Seq Integer -> Integer
+intSeqSynonymLength Empty     = 0
+intSeqSynonymLength (_ :< xs) = 1 + intSeqSynonymLength xs
+intSeqSynonymLength _         = 0
+
+intSeqLongFormLength :: Seq Integer -> Integer
+intSeqLongFormLength items
+  | S.null items = 0
+  | otherwise    = let _ S.:< xs = (S.viewl items)
+                   in              1 + intSeqLength xs
+
+sumEveryTwo :: [Integer] -> [Integer]
+sumEveryTwo []         = []     -- Do nothing to the empty list
+sumEveryTwo (x:[])     = [x]    -- Do nothing to lists with a single element
+sumEveryTwo (x:(y:zs)) = (x + y) : sumEveryTwo zs
+
+sumSeqEveryTwo :: Seq Integer -> Seq Integer
+sumSeqEveryTwo (S.viewl -> S.EmptyL)                        = empty       -- Do nothing to the empty seq
+sumSeqEveryTwo (S.viewl -> x S.:< (S.viewl -> S.EmptyL))    = x <| empty  -- Do nothing to seqs with a single element
+sumSeqEveryTwo (S.viewl -> (x S.:< (S.viewl -> y S.:< zs))) = (x + y) <| sumSeqEveryTwo zs
+sumSeqEveryTwo _                                            = empty
+
+sumSeqLongFormEveryTwo :: Seq Integer -> Seq Integer
+sumSeqLongFormEveryTwo items
+  | S.null items = empty       -- Do nothing to the empty seq
+  | otherwise    = let x S.:< xs = (S.viewl items)
+                   in  if S.null xs
+                         then x <| empty
+                         else let y S.:< zs = (S.viewl xs)
+                              in  (x + y) <| sumSeqEveryTwo zs
+
+-- The number of hailstone steps needed to reach 1 from a starting
+-- number.
+hailstoneLen :: Integer -> Integer
+hailstoneLen n = intListLength (hailstoneList n) - 1
+
+hailstoneSeqLen :: Integer -> Integer
+hailstoneSeqLen n = intSeqLength (hailstoneSeq n) - 1
