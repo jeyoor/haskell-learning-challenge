3f559b29da41ab2ef81147cd57c98d1f0db9f49b
Jeyan Oorjitham <jeyoor@gmx.com>
2017-06-23 22:27:39 -0400

    this build is failing, type sig not matching on worker

--- learn_haskell_github_courses/Cis194/src/Homework01.hs
+++ learn_haskell_github_courses/Cis194/src/Homework01.hs
@@ -7,8 +7,19 @@ module Homework01 (
     toDigitsRev,
     ) where
 
+-- | Convert a number [like 1234] into a list of individual digits [like [1, 2, 3, 4]
 toDigits :: Integer -> [Integer]
-toDigits num = [num]
+toDigits num = toDigitsWorker [] num 1
 
+-- | Recursive helper for toDigits.
+toDigitsWorker :: [Integer] -> Integer -> Integer -> [Integer]
+toDigitsWorker list num place
+  | num == 0   = list
+  | otherwise  = toDigitsWorker digit:list (num - remainder) place*10
+  where
+    remainder = num `mod` (place*10)
+    digit     = remainder / place
+
+-- | Convert a number [like 1234] into a list of individual reversed digits [like [4, 3, 2, 1]
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev num = [num]
