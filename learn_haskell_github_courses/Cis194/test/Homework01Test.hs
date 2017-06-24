062775994e5e2e326aae4ff162349e70c3b251ce
Jeyan Oorjitham <jeyoor@gmx.com>
2017-06-24 12:33:54 -0400

    forgot to add tests last time, added now, tests failing

--- learn_haskell_github_courses/Cis194/test/Homework01Test.hs
+++ learn_haskell_github_courses/Cis194/test/Homework01Test.hs
@@ -33,10 +33,34 @@ testToDigitsRev1 = TestCase (assertEqual "for (toDigitsRev (1234))," ([4, 3, 2, 
 testToDigitsRev2 :: Test
 testToDigitsRev2 = TestCase (assertEqual "for (toDigitsRev (-17))," ([]) (toDigitsRev (-17)))
 
-hTests = TestList [TestLabel "Test toDigits pos"    testToDigits1,
-                   TestLabel "Test toDigits neg"    testToDigits2,
-                   TestLabel "Test toDigitsRev pos" testToDigitsRev1,
-                   TestLabel "Test toDigitsRev neg" testToDigitsRev2]
+testDoubleEveryOther1 :: Test
+testDoubleEveryOther1 = TestCase (assertEqual "for (doubleEveryOther [1, 2, 3])," ([1, 4, 3]) (doubleEveryOther [1, 2, 3]))
+
+testDoubleEveryOther2 :: Test
+testDoubleEveryOther2 = TestCase (assertEqual "for (doubleEveryOther [8, 7, 6, 5])," ([16, 7, 12, 5]) (doubleEveryOther [8, 7, 6, 5]))
+
+testSumDigits1 :: Test
+testSumDigits1 = TestCase (assertEqual "for (sumDigits [16, 7, 12 ,5])," 22 (sumDigits [16, 7, 12, 5]))
+
+testSumDigits2 :: Test
+testSumDigits2 = TestCase (assertEqual "for (sumDigits [])," (0) (sumDigits []))
+
+testValidate1 :: Test
+testValidate1 = TestCase (assertEqual "for (validate 4012888888881881)," True (validate 4012888888881881))
+
+testValidate2 :: Test
+testValidate2 = TestCase (assertEqual "for (validate 4012888888881882)," False (validate 4012888888881882))
+
+hTests = TestList [TestLabel "Test toDigits pos"      testToDigits1,
+                   TestLabel "Test toDigits neg"      testToDigits2,
+                   TestLabel "Test toDigitsRev pos"   testToDigitsRev1,
+                   TestLabel "Test toDigitsRev neg"   testToDigitsRev2,
+                   TestLabel "Test doubleEveryOther1" testDoubleEveryOther1,
+                   TestLabel "Test doubleEveryOther2" testDoubleEveryOther2,
+                   TestLabel "Test sumDigits"         testSumDigits1,
+                   TestLabel "Test sumDigits empty"   testSumDigits2,
+                   TestLabel "Test validate1"         testValidate1,
+                   TestLabel "Test validate2"         testValidate2]
 --QuickCheck Tests
 
 prop_myReverse nums = myReverse nums == reverse nums
