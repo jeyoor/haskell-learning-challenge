aac30a94620124f23b7f9fee5c07c7f651ea3bb0
Jeyan Oorjitham <jeyoor@gmx.com>
2017-07-01 00:31:20 -0400

    unwords beats head! time to start chopping

--- learn_haskell_github_courses/Cis194/src/Homework02.hs
+++ learn_haskell_github_courses/Cis194/src/Homework02.hs
@@ -7,14 +7,49 @@ import Log
 parseMessage :: String -> LogMessage
 parseMessage message = LogMessage messageType 1312 "Lorem Ipsum"
   where chunks             = words message
-        code               = (head . take 1) chunks
+        code               = (unwords . take 1) chunks
+        errorString        = (unwords . take 1 . drop 1) chunks
         errorLevel  :: Int
         errorLevel         = case code of
-                                "E" -> (read . head . take 1 . drop 1) chunks
+                                "E" -> (read . unwords . take 1 . drop 1) chunks
                                 (_) -> 0
         messageType :: MessageType
         messageType = case code of
                          "E" -> Error errorLevel
                          "I" -> Info
                          "W" -> Warning
-                         --TODO: how to make this exhaustive?
+--TODO: how to make this exhaustive? How to do error handling?
+
+--Possible experiments/exploration of Maybe approach below
+
+-- | Maybe retrieve first item from a list 
+grabFirst :: [a] -> Maybe a
+grabFirst (x:_) = Just x
+grabFirst (_)   = Nothing
+
+-- | Maybe retrieve second item from a list 
+grabSecond :: [a] -> Maybe a
+grabSecond (x:_:_) = Just x
+grabSecond (_)     = Nothing
+
+-- | Maybe retrieve third item from a list 
+grabThird :: [a] -> Maybe a
+grabThird (x:_:_:_) = Just x
+grabThird (_)       = Nothing
+
+-- | Maybe convert a String to an Int
+convertInt :: Maybe String -> Maybe Int
+--TODO: use safe read here
+convertInt (Just a) = Just (read a)
+convertInt (_)      = Nothing
+
+-- | Create data?
+handleCode :: Maybe String -> Maybe String -> Maybe String -> LogMessage
+handleCode Nothing _ _           = Unknown ""
+handleCode (Just code) Nothing _ = Unknown code
+handleCode (Just code) _ Nothing = Unknown code
+handleCode (Just code) (Just time) errorLevel = case code of
+                                                 -- "E" -> LogMessage
+                                                 -- "I" -> LogMessage 
+                                                 -- "W" -> LogMessage
+                                                  (_) -> Unknown code
