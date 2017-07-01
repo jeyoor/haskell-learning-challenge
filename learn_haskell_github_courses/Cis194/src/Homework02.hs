a805c17ddf728067fd3d0b60cbb74b20c9820b17
Jeyan Oorjitham <jeyoor@gmx.com>
2017-06-30 23:57:41 -0400

    partway through implementing parseMessage

--- learn_haskell_github_courses/Cis194/src/Homework02.hs
+++ learn_haskell_github_courses/Cis194/src/Homework02.hs
@@ -5,4 +5,16 @@ import Log
 
 -- | Parse a line of the log into the structured format
 parseMessage :: String -> LogMessage
-parseMessage _ = LogMessage Info 9001 "Lorem Ipsum"
+parseMessage message = LogMessage messageType 1312 "Lorem Ipsum"
+  where chunks             = words message
+        code               = (head . take 1) chunks
+        errorLevel  :: Int
+        errorLevel         = case code of
+                                "E" -> (read . head . take 1 . drop 1) chunks
+                                (_) -> 0
+        messageType :: MessageType
+        messageType = case code of
+                         "E" -> Error errorLevel
+                         "I" -> Info
+                         "W" -> Warning
+                         --TODO: how to make this exhaustive?
