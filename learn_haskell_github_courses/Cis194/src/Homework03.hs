cbf3e7391c90bb3c0799749f92744da4417cb20c
Jeyan Oorjitham <jeyoor@gmx.com>
2017-08-05 18:44:39 -0400

    wrote working/correct skips

--- learn_haskell_github_courses/Cis194/src/Homework03.hs
+++ learn_haskell_github_courses/Cis194/src/Homework03.hs
@@ -3,5 +3,35 @@ module Homework03 (
     skips,
     ) where
 
+import Data.List(length)
+
+-- | Recursively builds a list of sublists, picking every nth item
 skips :: [a] -> [[a]]
-skips l = [l]
+skips [] = [[]]
+skips l = skipsWorker 1 (replicate (length l) l) []
+
+-- | Recursively builds a list of sublists, picking every nth item
+skipsWorker :: Int -> [[a]] -> [[a]] -> [[a]]
+skipsWorker _ [] output = reverse output
+skipsWorker num (x:xs) output = skipsWorker (num + 1) xs ((skip num x):output)
+
+--TODO: replace above function with mapAccumL
+--Examples working: 
+--(\x y -> (x + y, y) ) 3 [1, 2, 3, 4, 5, 6]
+--(24,[1,2,3,4,5,6])
+--mapAccumL (\x y -> (take y x, y + 1) ) 3 ["abcd", "abcdef", "abcdefghi", "abcdefghi", "abcdefghij", "abcdefghijk"]
+--mapAccumL (\x y -> (x + ord y, show x ++ [y]) ) 3 "abcdefg"
+--Examples failing:
+--mapAccumL (\x y -> (show x ++ y, x + 1) ) 3 ["abcdefg"]
+--(703,["3a","100b","198c","297d","397e","498f","600g"])
+
+-- | Given an index and a list, return the sublist of every indexth item
+skip :: Int -> [a] -> [a]
+skip base fullList  = skipWorker base 1 fullList []
+
+-- | recursive worker for the skip function
+skipWorker :: Int -> Int -> [a] -> [a] -> [a]
+skipWorker base _ [] newlist = reverse newlist
+skipWorker base idx (x:xs) newlist = if idx `mod` base == 0
+                                     then skipWorker base (idx + 1) xs (x:newlist)
+                                     else skipWorker base (idx + 1) xs newlist
